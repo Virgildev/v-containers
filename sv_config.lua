2db3e1ac2dd330c487dc03732a70a7d7f993b925
Virgil <157654563+Virgildev@users.noreply.github.com>
2025-07-03 20:47:06 -0700

    Update sv_config.lua

--- sv_config.lua
+++ sv_config.lua
@@ -1,6 +1,6 @@
 SV_Config = {}
 
-SV_Config.Webhook = 'https://discord.com/api/webhooks/1316133731408281640/sXaAq3Xth8gnWCzuedaWGpTYeXCs7xm1uhXv6gH4-Q884tGjQgMo3LRpTW9BOtcy0VG1' -- ur webhook here for logging
+SV_Config.Webhook = '' -- ur webhook here for logging
 
 SV_Config.LogSettings = {
     placeContainer = true,
